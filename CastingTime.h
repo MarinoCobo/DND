301b8f3af164f4c31abe5d52ebf7fa54ae3fc65c
Marino Cobo <josemartincoboh@gmail.com>
2026-04-21 23:29:15 -0500

    Finished CastingTime.h

---
+++ CastingTime.h
@@ -0,0 +1,18 @@
+#ifndef CASTINGTIME_H
+#define CASTINGTIME_H
+
+#include "DNDTypes.h"
+
+class CastingTime
+{
+    private:
+        int amount;
+        TimeUnit unit;
+    
+    public:
+        CastingTime(int amount, TimeUnit unit);
+        void displayInfo() const;
+        int getAmount() const;
+        TimeUnit getTimeUnit() const;
+};
+#endif
